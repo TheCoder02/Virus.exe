d78cb5096e43c209749f97dcb37b8a43a9ccb63d
Lawson <97492153+TheCoder02@users.noreply.github.com>
2023-05-12 08:31:44 -0500

    Update notifications.py
    updated files

--- notifications.py
+++ notifications.py
@@ -1,23 +1,40 @@
 #notifications library, runs notifications
-"""
-#import plyer
-#import pillow
+
+from plyer import *
+from PIL import Image
 import pyautogui
 from Graphics import *
+import socket
+
+"""
+def targetScreen():
+   im = Image.open("6-0Win.png")
+   im.show()   
+   im2 = Image.open("dunkedon.png")
+   im2.show()
+   im3 = Image.open("obama.png")
+   im3.show()
+   im4 = Image.open("davy1kill.png")
+   im4.show()
+   im5= Image.open("")
+   im5.show()
+   im6 = Image.open()
+"""
+   
+   
+   
+def ip():
+   hostname = socket.gethostname()
+   ip_address = socket.gethostbyname(hostname)
+   print(f"Hostname: {hostname}")
+   print(f"IP Address: {ip_address}")
+
 
 def virusClick():
    notification.notify(
       title = 'Target Sale',
       message = 'Don\'t miss these weekly ad deals.',
-      app_icon = r'amc.ico',
+      app_icon = 'amc.ico',
       timeout = 10,
    )
 
-x= 0
-y= 0
-def settings():
-   if (x,y,600,300,200,500):
-      while penup() == True:
-         break
-      
-"""
